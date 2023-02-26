e27193f796e81dbd69a5f3f4f7dee97c47496d3f
Otávio Augusto <tavinteixeira@hotmail.com>
2023-02-26 15:51:59 -0300

    Carousel com nomes e cargos / ordem de imagens arrumadas / festa -> eventos

--- src/components/Equipe/index.js
+++ src/components/Equipe/index.js
@@ -46,7 +46,11 @@ const Equipe = () => {
                     >
                         {jsonData.equipe.map((e, index) => (
                             <SwiperSlide key={index}>
-                                <Image src={`./equipe/${e}`}  alt={`image_${e}`} />
+                                <Image src={`./equipe/${e.location}`}  alt={`image_${e.location}`} />
+                                <Flex align="center" justify="center" flexDir="column">
+                                    <Text textAlign="center" fontSize={["md","lg"]} color="white">{e.nome}</Text>
+                                    <Text textAlign="center" fontSize={["sm","md"]} color="white">{e.funcao}</Text>
+                                </Flex>
                             </SwiperSlide>
                         ))}
                     </Swiper>
--- src/components/Festas/index.js
+++ src/components/Festas/index.js
@@ -7,19 +7,23 @@ const Festas = () => {
         <Flex id="festas" w="100vw" mt="11.25rem">
             <Container  maxW={["22rem","90rem"]} margin="auto" padding="2.5 16" style={{ userSelect: "none" }}>
             <Flex flexDir="column" align="center" justify="center">
-                <Text fontWeight="700" fontSize={["4xl","5xl"]} letterSpacing={["0.2rem",""]} color="white">FESTAS</Text>
+                <Text fontWeight="700" fontSize={["4xl","5xl"]} letterSpacing={["0.2rem",""]} color="white">EVENTOS</Text>
                 <Flex w="100%" mt="20" align="center" justify="space-between" flexDir={["column","column","column","row"]}>
                     <Flex w={["21rem","25rem"]} h="37.5rem" justify="space-evenly" align="center" flexDir="column">
-                        <Image w={["21rem","25rem"]} h="auto" src="./festas/img_computaria.png" alt="imagem_festa_1" />
+                        <Image w={["19rem","20rem"]} h="auto" src="./festas/img_computaria.png" alt="imagem_festa_1" />
                         <Text textTransform="uppercase" align="center" color="white" fontWeight="400" fontSize="lg">COMPUTARIA, 2022</Text>
                     </Flex>
                     <Flex w={["21rem","25rem"]} h="37.5rem" justify="space-evenly" align="center" flexDir="column">
-                        <Image w={["21rem","25rem"]} h="auto" src="./festas/img_trickortrad.png" alt="imagem_festa_2" />
+                        <Image w={["19rem","20rem"]} h="auto" src="./festas/img_trickortrad.png" alt="imagem_festa_2" />
                         <Text textTransform="uppercase" align="center" color="white" fontWeight="400" fontSize="lg">TRICK OR TRAD, 2022</Text>
                     </Flex>
                     <Flex w={["21rem","25rem"]} h="37.5rem" justify="space-evenly" align="center" flexDir="column">
-                        <Image w={["21rem","25rem"]} h="auto" src="./festas/img_chernobyte.png" alt="imagem_festa_3" />
+                        <Image w={["19rem","20rem"]} h="auto" src="./festas/img_chernobyte.png" alt="imagem_festa_3" />
                         <Text textTransform="uppercase" align="center" color="white" fontWeight="400" fontSize="lg">CHERNOBYTE, 2022</Text>
+                    </Flex>
+                    <Flex w={["21rem","25rem"]} h="37.5rem" justify="space-evenly" align="center" flexDir="column">
+                        <Image w={["19rem","20rem"]} h="auto" src="./festas/foto_eventos_1.jpeg" alt="foto_eventos_1.jpeg" />
+                        <Text textTransform="uppercase" align="center" color="white" fontWeight="400" fontSize="lg">MARATONA SBC, 2022</Text>
                     </Flex>
                 </Flex>
             </Flex>
