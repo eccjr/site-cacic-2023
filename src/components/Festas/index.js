59d3ddb3c8e24e8ee6f73165cecaaffddf041e49
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2023-03-26 13:04:12 -0300

    Adicionando foto do churrasco da gestão

--- src/components/Festas/index.js
+++ src/components/Festas/index.js
@@ -8,7 +8,13 @@ const Festas = () => {
             <Container  maxW={["22rem","90rem"]} margin="auto" padding="2.5 16" style={{ userSelect: "none" }}>
             <Flex flexDir="column" align="center" justify="center">
                 <Text fontWeight="700" fontSize={["4xl","5xl"]} letterSpacing={["0.2rem",""]} color="white">EVENTOS</Text>
-                <Flex w="100%" mt="20" align="center" justify="space-between" flexDir={["column","column","column","row"]}>
+                <Flex w="100%" mt="20" align="center" justify="center">
+                    <Flex w={["21rem","25rem"]} h="23rem" justify="space-evenly" align="center" flexDir="column">
+                        <Image w={["79rem","80rem"]} h="auto" src="./festas/churrasco.webp" alt="imagem_churrasco" />
+                        <Text textTransform="uppercase" align="center" color="white" fontWeight="400" fontSize="lg" mt={35}>CHURRASCO DA GESTÃO LOVELACE, 2023</Text>
+                    </Flex>
+                </Flex>
+                <Flex w="100%" mt="18" align="center" justify="space-between" flexDir={["column","column","column","row"]}>
                     <Flex w={["21rem","25rem"]} h="37.5rem" justify="space-evenly" align="center" flexDir="column">
                         <Image w={["19rem","20rem"]} h="auto" src="./festas/img_computaria_1.webp" alt="imagem_festa_1" />
                         <Text textTransform="uppercase" align="center" color="white" fontWeight="400" fontSize="lg">COMPUTARIA, 2022</Text>
